f542eb46fa2368a4c5db3a55a3d8d9a923b3dd6f
kazu <64774307+kazu-2020@users.noreply.github.com>
2025-11-15 06:06:23 +0000

    feat: add time support for SQLite ADBC adapter and update tests

--- lib/activerecord_adbc_adapter/quoting.rb
+++ lib/activerecord_adbc_adapter/quoting.rb
@@ -13,7 +13,49 @@ module ActiveRecordADBCAdapter
     end
 
     def quoted_time(value)
-      value
+      case backend
+      when "sqlite"
+        sqlite3_quoting_proxy.quoted_time(value)
+      else
+        value
+      end
+    end
+
+    private
+
+    def sqlite3_quoting_proxy
+      @_sqlite3_quoting_proxy ||= begin
+        require 'active_record/connection_adapters/sqlite3/quoting'
+
+        # Dynamically create proxy class to avoid loading SQLite3 module at file load time
+        proxy_class = Class.new do
+          include ActiveRecord::ConnectionAdapters::Quoting
+          include ActiveRecord::ConnectionAdapters::SQLite3::Quoting
+
+          def initialize(adapter)
+            @adapter = adapter
+          end
+
+          # Delegate methods to the ADBC adapter (for default_timezone, etc.)
+          def method_missing(method, *args, &block)
+            if adapter.respond_to?(method)
+              adapter.public_send(method, *args, &block)
+            else
+              super
+            end
+          end
+
+          def respond_to_missing?(method, include_private = false)
+            adapter.respond_to?(method, include_private) || super
+          end
+
+          private
+
+          attr_reader :adapter
+        end
+
+        proxy_class.new(self)
+      end
     end
   end
 end
--- lib/activerecord_adbc_adapter/result.rb
+++ lib/activerecord_adbc_adapter/result.rb
@@ -37,6 +37,26 @@ module ActiveRecordADBCAdapter
             when Arrow::StringDataType
               casted_type = Arrow::TimestampDataType.new(:nano)
             end
+          when :time
+            case field.data_type
+            when Arrow::StringDataType
+              # Parse time strings from SQLite's format (2000-01-01 HH:MM:SS.SSSSSS) and convert to Time64Array
+              # Format is defined in Rails' SQLite3 adapter:
+              # https://github.com/rails/rails/blob/90a1eaa1b30ba1f2d524e197460e549c03cf5698/activerecord/lib/active_record/connection_adapters/sqlite3/quoting.rb#L74-L77
+              time_values = column.data.chunks.flat_map do |chunk|
+                chunk.collect do |time_str|
+                  if time_str.nil?
+                    nil
+                  else
+                    dt = time_str.to_time
+                    (dt.seconds_since_midnight * 1_000_000).to_i
+                  end
+                end
+              end
+              chunked_array = Arrow::ChunkedArray.new([Arrow::Time64Array.new(:micro, time_values)])
+              field = Arrow::Field.new(field.name, Arrow::Time64DataType.new(:micro))
+              casted = true
+            end
           end
           if casted_type
             chunked_array = column.cast(casted_type)
